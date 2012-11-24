a91b1bf9952a40b07c6bda70207e3d92f99463b5
Alan Aguiar <alanjas@hotmail.com>
2012-11-24 04:14:49 -0200

    add commons.py file

---
+++ conozco-uruguay.activity/recursos/comun/datos/commons.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+ACTIVITY_NAME = _('I know Uruguay')
+
+PRESENTATION = [
+    _('¡Qué hermoso planeta!\nVoy a explorar aquella penillanura\ncontra el océano'),
+    _('¡Oh, no!\nLa nave tiene problemas\n¡Me voy a estrellar!'),
+    _('¡Por suerte me salvé!\nAhora tengo que reconstruir\nmi nave, pero no conozco\neste lugar. ¡Necesito ayuda!')
+]
+
+PREFIX = [
+    _('We have to find'),
+    _('Now we have to find'),
+    _('We need to find')
+]
+
+SUFIX = [
+    _('Can you tell me where it is?'),
+    _('Where is it?'),
+    _('Can you show me where it is?'),
+    _('Can you find it?')
+]
+
+CORRECT = [
+    _('Very well!'),
+    _('Brilliant!'),
+    _('You found it!'),
+    _('Yes!')
+]
+
+WRONG = [
+    _('No, that is not correct'),
+    _('No, it is not there'),
+    _('No, that seems to be wrong'),
+    _('That is not correct')
+]
+
+BYE_C = [
+    _("Now I'm ready\nfor tomorrow...\nI'm going to play...\nThanks for your help!"),
+    _("We made it!\nI'm going to play ball...\nBye and thanks\nfor helping me!"),
+    _("We did it!\nI'm ready for tomorrow...\nI'm playing a while...\nUntil next time!")
+]
+
+BYE_W = [
+    _("Better luck next time\nI'll play a while...\nThanks!"),
+    _("The next time will be better\nI'm going to play ball...\nThanks!"),
+    _("Please try again...\nI'm going to play...\nThanks!")
+]
+
+CREDITS = [
+    _("Autor: Gabriel Eirea"),
+    _("Ayudante: Fabio Eirea"),
+    _("Por favor enviar comentarios y sugerencias a geirea@gmail.com"),
+    "",
+    _("Este programa es software libre y desarrollado por la comunidad"),
+    _("Por más información visitá http://ceibaljam.org"),
+    "",
+    _("Gráficos y niveles para todos los departamentos menos Canelones: Alan Aguiar"),
+    _("Información para el nivel 'Lugares históricos' de Uruguay: Ana Trujillo y Ana Cichero (coordinación)"),
+    _("Información para Canelones: Nancy Gómez, su hijo Matías y Ana Cichero (coordinación)"),
+    _("Información para el nivel 'Información de Montevideo': Miriam Martínez y Magdalena Lallo"),
+    _("Sonidos bajados de freesound.org: btn045.wav por junggle y boom1.wav por de NoiseCollector"),
+    _("Fuentes bajadas de urbanfonts.com: AllCaps.ttf por BobFrantic y Share-Regular.ttf por anónimo")
+]
+
