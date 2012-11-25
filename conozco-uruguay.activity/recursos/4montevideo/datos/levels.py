49e6614b7e5de6365b553dfa44c919cc1f11611e
Alan Aguiar <alanjas@hotmail.com>
2012-11-25 20:31:04 -0200

    change paradigm in montevideo

---
+++ conozco-uruguay.activity/recursos/4montevideo/datos/levels.py
@@ -0,0 +1,136 @@
+﻿# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL1 = [
+        1,
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('the neighborhoods of %s') % _('Santiago Vázquez'), 2, _('Santiago Vázquez'), _('Queda en el límite\ncon San José')),
+    (_('the neighborhoods of %s') % _('Ciudad Vieja'), 2, _('Ciudad Vieja'), _('Es donde se fundó\nla ciudad')),
+    (_('the neighborhoods of %s') % _('Centro'), 2, _('Centro'), _('Queda al lado de la\nCiudad Vieja')),
+    (_('the neighborhoods of %s') % _('Sur'), 2, _('Sur'), _('Queda al sur')),
+    (_('the neighborhoods of %s') % _('Palermo'), 2, _('Palermo'), _('Queda al sur')),
+    (_('the neighborhoods of %s') % _('Cordón'), 2, _('Cordón'), _('Queda cerca del Centro')),
+    (_('the neighborhoods of %s') % _('Aguada'), 2, _('Aguada'), _('Queda sobre la bahía')),
+    (_('the neighborhoods of %s') % _('Punta Carretas'), 2, _('Punta Carretas'), _('Es el extremo más\nal sur de la ciudad')),
+    (_('the neighborhoods of %s') % _('Pocitos'), 2, _('Pocitos'), _('Tiene una playa')),
+    (_('the neighborhoods of %s') % _('Buceo'), 2, _('Buceo'), _('Tiene una playa')),
+    (_('the neighborhoods of %s') % _('Malvín'), 2, _('Malvín'), _('Tiene una playa')),
+    (_('the neighborhoods of %s') % _('Punta Gorda'), 2, _('Punta Gorda'), _('Tiene una playa')),
+    (_('the neighborhoods of %s') % _('Carrasco'), 2, _('Carrasco'), _('Queda bien al este')),
+    (_('the neighborhoods of %s') % _('Parque Batlle'), 2, _('Parque Batlle'), _('Es donde está el\nEstadio Centenario')),
+    (_('the neighborhoods of %s') % _('Unión'), 2, _('Unión'), _('Queda al centroeste')),
+    (_('the neighborhoods of %s') % _('Maroñas'), 2, _('Maroñas'), _('Queda cerca de la Unión')),
+    (_('the neighborhoods of %s') % _('Villa Española'), 2, _('Villa Española'), _('Queda cerca de la Unión')),
+    (_('the neighborhoods of %s') % _('Capurro'), 2, _('Capurro'), _('Queda sobre la bahía')),
+    (_('the neighborhoods of %s') % _('Prado'), 2, _('Prado'), _('Queda al norte de la bahía')),
+    (_('the neighborhoods of %s') % _('Belvedere'), 2, _('Belvedere'), _('Queda cerca del Prado')),
+    (_('the neighborhoods of %s') % _('Cerro'), 2, _('Cerro'), _('Queda sobre la bahía')),
+    (_('the neighborhoods of %s') % _('Casabó'), 2, _('Casabó'), _('Queda cerca del Cerro')),
+    (_('the neighborhoods of %s') % _('La Teja'), 2, _('La Teja'), _('Queda sobre la bahía')),
+    (_('the neighborhoods of %s') % _('Nuevo París'), 2, _('Nuevo París'), _('Queda al noroeste')),
+    (_('the neighborhoods of %s') % _('Sayago'), 2, _('Sayago'), _('Queda al norte')),
+    (_('the neighborhoods of %s') % _('Peñarol'), 2, _('Peñarol'), _('Queda al norte')),
+    (_('the neighborhoods of %s') % _('Colón'), 2, _('Colón'), _('Queda al norte')),
+    (_('the neighborhoods of %s') % _('Casavalle'), 2, _('Casavalle'), _('Queda al norte')),
+    (_('the neighborhoods of %s') % _('Cerrito de la Victoria'), 2, _('Cerrito de la Victoria'), _('Queda en el centronorte')),
+    (_('the neighborhoods of %s') % _('Jardines del Hipódromo'), 2, _('Jardines del Hipódromo'), _('Queda al noreste')),
+    (_('the neighborhoods of %s') % _('Piedras Blancas'), 2, _('Piedras Blancas'), _('Queda al norte')),
+    (_('the neighborhoods of %s') % _('Punta de Rieles'), 2, _('Punta de Rieles'), _('Queda al noreste')),
+    (_('the neighborhoods of %s') % _('Brazo Oriental'), 2, _('Brazo Oriental'), _('Queda cerca del Prado')),
+    (_('the neighborhoods of %s') % _('Jacinto Vera'), 2, _('Jacinto Vera'), _('Queda al norte del Cordón')),
+    (_('the neighborhoods of %s') % _('Lezica'), 2, _('Lezica'), _('Queda cerca de Colón')),
+    (_('the neighborhoods of %s') % _('Paso de la Arena'), 2, _('Paso de la Arena'), _('Queda al oeste')),
+    (_('the neighborhoods of %s') % _('Villa García'), 2, _('Villa García'), _('Queda al noreste')),
+    (_('the neighborhoods of %s') % _('Manga'), 2, _('Manga'), _('Queda al norte'))
+]
+]
+
+LEVEL2 = [
+        1,
+        _('Information'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        ['capitales', 'ciudades'],
+[
+    (_('el punto más elevado\ndel departamento'), 5, _('Co. de Montevideo'), _('Es el símbolo\ndel departamento')),
+    (_('el segundo punto más elevado\ndel departamento'), 2, _('Cerrito de la Victoria'), _('Es un barrio muy popular')),
+    (_('el arroyo que limita al este\ncon Canelones'), 3, _('A. Carrasco'), _('Desemboca en el\nRío de la Plata')),
+    (_('el barrio donde originariamente\nse fundó la ciudad de San Felipe\ny Santiago de Montevideo'), 2, _('Ciudad Vieja'), _('Queda sobre la bahía')),
+    (_('el barrio donde funcionaban\nlos talleres de ferrocarril'), 2, _('Peñarol'), _('Un importante cuadro\nde fútbol lleva su nombre')),
+    (_('el barrio que debe su nombre\na la zona en donde existían\nnumerosos pozos de agua'), 2, _('Aguada'), _('Es donde actualmente está\nubicado el Palacio Legislativo')),
+    (_('la playa donde las\nlavanderas acudían a lavar\nla ropa haciendo pozos'), 2, _('Pocitos'), _('Es una playa muy concurrida')),
+    (_('el barrio desde donde\nsale el desfile de Llamadas'), 2, _('Sur'), _('Queda al sur')),
+    (_('donde se encuentra el\n"Monumento del Fútbol Mundial"'), 2, _('Parque Batlle'), _('El "Monumento del Fútbol Mundial"\nes el Estadio Centenario')),
+    (_('el río que abastece de\nagua potable al departamento\nde Montevideo'), 3, _('Río Santa Lucía'), _('Queda en el límite\ncon San José'))
+]
+]
+	
+LEVEL3 = [
+        1,
+        _('Waterways'),
+        ['lineasDepto', 'rios'],
+        [],
+[
+    (_('the %s') % _('Río de la Plata'), 3, _('Río de la Plata'), _("It's in the south")),
+    (_('the %s') % _('Río Santa Lucía'), 3, _('Río Santa Lucía'), _('Está en el límite\ncon San José')),
+    (_('the %s') % _('Arroyo Colorado'), 3, _('A. Colorado'), _('Desemboca en el\nRío Santa Lucía')),
+    (_('the %s') % _('Arroyo de Melilla'), 3, _('A. de Melilla'), _('Desemboca en el\nRío Santa Lucía')),
+    (_('the %s') % _('Arroyo San Gregorio'), 3, _('A. San Gregorio'), _('Desemboca en el\nRío Santa Lucía')),
+    (_('the %s') % _('Arroyo Pantanoso'), 3, _('A. Pantanoso'), _('Desemboca en la\nBahía de Montevideo')),
+    (_('the %s') % _('Arroyo Miguelete'), 3, _('A. Miguelete'), _('Desemboca en la\nBahía de Montevideo')),
+    (_('the %s') % _('Arroyo Mendoza'), 3, _('A. Mendoza'), _('Desemboca en el\nArroyo Miguelete')),
+    (_('the %s') % _('Arroyo de Las Piedras'), 3, _('A. de las Piedras'), _('Está en el límite\ncon Canelones')),
+    (_('the %s') % _('Arroyo Toledo'), 3, _('A. Toledo'), _('Está en el límite\ncon Canelones')),
+    (_('the %s') % _('Arroyo Carrasco'), 3, _('A. Carrasco'), _("It's in the east")),
+    (_('the %s') % _('Arroyo Manga'), 3, _('A. Manga'), _('Queda al noreste'))
+]
+]
+
+LEVEL4 = [
+        1,
+        _('Elevations'),
+        ['cuchillas', 'cerros'],
+        [],
+[
+    (_('the %s') % _('Cuchilla Grande'), 4, _('Cuchilla Grande'), _('Dále, probá de nuevo')),
+    (_('the %s') % _('Cabo de Pereira'), 4, _('Cabo de Pereira'), _('Dále, probá de nuevo')),
+    (_('the %s') % _('Cerro de Montevideo'), 5, _('Co. de Montevideo'), _('Queda sobre la bahía'))
+]
+]
+
+LEVEL5 = [
+        1,
+        _('Avenues and routes'),
+        ['rutas', 'capitales'],
+        ['capitales', 'ciudades'],
+[
+    (_('la Avenida\n18 de Julio'), 6, _('18 de Julio'), _('Probá otro')),
+    (_('Boulevard Artigas'), 6, _('Bvar. Artigas'), _('Probá otro')),
+    (_('la Avenida Rivera'), 6, _('Rivera'), _('Probá otro')),
+    (_('Avenida Italia'), 6, _('Av. Italia'), _('Probá otro')),
+    (_('Camino Carrasco'), 6, _('Cno. Carrasco'), _('Probá otro')),
+    (_('Camino Maldonado'), 6, _('Cno. Maldonado'), _('Probá otro')),
+    (_('la Avenida\n8 de Octubre'), 6, _('8 de Octubre'), _('Probá otro')),
+    (_('la Avenida Belloni'), 6, _('Belloni'), _('Probá otro')),
+    (_('Camino Mendoza'), 6, _('Mendoza'), _('Probá otro')),
+    (_('la Avenida\nGeneral Flores'), 6, _('Gral. Flores'), _('Probá otro')),
+    (_('la Avenida J. Batlle\ny Ordóñez (Propios)'), 6, _('Propios'), _('Probá otro')),
+    (_('la Avenida de las\nInstrucciones'), 6, _('Instrucciones'), _('Probá otro')),
+    (_('la Avenida Agraciada'), 6, _('Agraciada'), _('Probá otro')),
+    (_('la Avenida Garzón'), 6, _('Garzón'), _('Probá otro')),
+    (_('los Accesos de las\Rutas 1 y 5'), 6, _('Accesos'), _('Probá otro')),
+    (_('Ruta 1'), 6, _('Ruta 1'), _('Probá otro')),
+    (_('Ruta 5'), 6, _('Ruta 5'), _('Probá otro')),
+    (_('la Avenida Luis\nB. Berres'), 6, _('Luis B. Berres'), _('Probá otro')),
+    (_('Camino Sanguinetti'), 6, _('Cno. Sanguinetti'), _('Probá otro')),
+    (_('Camino Tomkinson'), 6, _('Cno. Tomkinson'), _('Probá otro')),
+    (_('Camino Cibils'), 6, _('Cno. Cibils'), _('Probá otro')),
+    (_('Camino Melilla'), 6, _('Cno. Melilla'), _('Probá otro'))
+]
+]
+
+LEVELS = [LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5]
+
