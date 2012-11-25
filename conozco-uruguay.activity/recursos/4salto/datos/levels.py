de90cd3078da24205e2b8b2060793ceca3024882
Alan Aguiar <alanjas@hotmail.com>
2012-11-25 20:36:49 -0200

    change paradigm in salto

---
+++ conozco-uruguay.activity/recursos/4salto/datos/levels.py
@@ -0,0 +1,102 @@
+﻿# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL2 = [
+        1,
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('the city of %s') % _('Salto'), 1, _('Salto'), _('Es la capital del\ndepartamento')),
+    (_('the city of %s') % _('Belén'), 2, _('Belén'), _("It's in the northwest")),
+    (_('the city of %s') % _('Constitución'), 2, _('Constitución'), _("It's in the west")),
+    (_('the town of %s') % _('Palomas'), 2, _('Palomas'), _("It's in the northwest")),
+    (_('the town of %s') % _('Colonia Lavalleja'), 2, _('Colonia Lavalleja'), _("It's in the north")),
+    (_('the town of %s') % _('Saucedo'), 2, _('Saucedo'), _("It's in the northwest")),
+    (_('the city of %s') % _('Masoller'), 2, _('Masoller'), _("It's in the northeast")),
+    (_('the town of %s') % _('Cayetano'), 2, _('Cayetano'), _("It's in the north")),
+    (_('the town of %s') % _('Biassini'), 2, _('Biassini'), _('Está en el centro')),
+    (_('the town of %s') % _('Rincón de Valentín'), 2, _('Rincón de Valentín'), _('Está en el centro')),
+    (_('the town of %s') % _('Celeste'), 2, _('Celeste'), _('Está en el centro')),
+    (_('the town of %s') % _('San Antonio'), 2, _('San Antonio'), _("It's in the southwest")),
+    (_('the town of %s') % _('Albisu'), 2, _('Albisu'), _("It's in the southwest")),
+    (_('the town of %s') % _('Laureles'), 2, _('Laureles'), _("It's in the southwest")),
+    (_('the town of %s') % _('Puntas de Valentín'), 2, _('Puntas de Valentín'), _('Está en el centro')),
+    (_('the town of %s') % _('Cerros de Vera'), 2, _('Cerros de Vera'), _("It's in the south")),
+    (_('the town of %s') % _('Paso del Parque'), 2, _('Paso del Parque'), _("It's in the south")),
+    (_('the town of %s') % _('Arerunguá'), 2, _('Arerunguá'), _("It's in the southeast")),
+    (_('the health resort of %s') % _('Termas del Arapey'), 2, _('Termas del Arapey'), _("It's in the northwest")),
+    (_('the town of %s') % _('Olivera'), 2, _('Olivera'), _("It's in the north")),
+    (_('the town of %s') % _('Mataojo'), 2, _('Mataojo'), _("It's in the northeast"))
+]
+]
+
+LEVEL3 = [
+        1,
+        _('Waterways'),
+        ['lineasDepto', 'rios'],
+        [],
+[
+    (_('the %s') % _('Río Uruguay'), 3, _('Río Uruguay'), _("It's in the west")),
+    (_('the %s') % _('Río Arapey Gr.'), 3, _('Río Arapey Gr.'), _("It's in the north")),
+    (_('the %s') % _('Río Arapey Ch.'), 3, _('Río Arapey Ch.'), _("It's in the north")),
+    (_('the %s') % _('Río Daymán'), 3, _('Río Daymán'), _("It's in the south")),
+    (_('the %s') % _('Arroyo Laureles Gr.'), 3, _('A. Laureles Gr.'), _("It's in the southwest")),
+    (_('the %s') % _('Arroyo Laureles Ch.'), 3, _('A. Laureles Ch.'), _("It's in the south")),
+    (_('the %s') % _('Arroyo de las Cañas'), 3, _('A. de las Cañas'), _('Está en el centro')),
+    (_('the %s') % _('Arroyo Sopas'), 3, _('A. Sopas'), _("It's in the east")),
+    (_('the %s') % _('Arroyo del Sauce'), 3, _('A. del Sauce'), _('Está en el centro')),
+    (_('the %s') % _('Arroyo S. Mataojito'), 3, _('A. S. Mataojito'), _("It's in the northeast")),
+    (_('the %s') % _('Arroyo Mataojo Gr.'), 3, _('A. Mataojo Gr.'), _("It's in the east")),
+    (_('the %s') % _('Arroyo Mataojo Ch.'), 3, _('A. Mataojo Ch.'), _("It's in the east")),
+    (_('the %s') % _('Arroyo Paloma Gr.'), 3, _('A. Paloma Gr.'), _("It's in the northwest")),
+    (_('the %s') % _('Arroyo Tangarupá'), 3, _('A. Tangarupá'), _("It's in the northwest")),
+    (_('the %s') % _('Arroyo Valentín Gr.'), 3, _('A. Valentín Gr.'), _('Está en el centro')),
+    (_('the %s') % _('Arroyo Arerunguá'), 3, _('. Arerunguá'), _("It's in the southeast")),
+    (_('the %s') % _('Arroyo Sarandí'), 3, _('A. Sarandí'), _("It's in the southeast")),
+    (_('the %s') % _('Arroyo Guayabos'), 3, _('A. Guayabos'), _("It's in the southeast")),
+    (_('the %s') % _('Arroyo Tapado'), 3, _('A. Tapado'), _("It's in the southeast")),
+    (_('the %s') % _('Arroyo de los Talas'), 3, _('A. de los Talas'), _('Está en el centro')),
+    (_('the %s') % _('Arroyo Valentín Ch.'), 3, _('A. Valentín Ch.'), _('Está en el centro')),
+    (_('the %s') % _('Arroyo Tunas'), 3, _('A. Tunas'), _("It's in the northwest")),
+    (_('the %s') % _('Arroyo S. del Arapey'), 3, _('A. S. del Arapey'), _("It's in the northeast")),
+    (_('the %s') % _('Arroyo Sarandí Ch.'), 3, _('A. Sarandí Ch.'), _("It's in the northeast")),
+    (_('the %s') % _('Arroyo Moisés'), 3, _('A. Moisés'), _("It's in the northeast")),
+    (_('the %s') % _('Arroyo Itapeby Gr.'), 3, _('A. Itapeby Gr.'), _("It's in the west")),
+    (_('the %s') % _('Arroyo Itapeby Ch.'), 3, _('A. Itapeby Ch.'), _("It's in the west")),
+    (_('the %s') % _('Arroyo San Antonio'), 3, _('A. San Antonio'), _("It's in the west")),
+    (_('the %s') % _('Arroyo Yacuy'), 3, _('A. Yacuy'), _("It's in the north"))
+]
+]
+
+LEVEL4 = [
+        1,
+        _('Elevations'),
+        ['cuchillas', 'cerros'],
+        [],
+[
+    (_('the %s') % _('Cuchilla de los Arapeyes'), 4, _('Cuchilla de los Arapeyes'), _('Dále, probá de nuevo')),
+    (_('the %s') % _('Cuchilla del Daymán'), 4, _('Cuchilla del Daymán'), _('Dále, probá de nuevo')),
+    (_('the %s') % _('Cuchilla de Haedo'), 4, _('Cuchilla de Haedo'), _('Dále, probá de nuevo')),
+    (_('the %s') % _('Cerro Fortaleza'), 5, _('Co. Fortaleza'), _('Queda en el centro')),
+    (_('the %s') % _('Cerro de Corumbé'), 5, _('Co. de Corumbé'), _('Queda al este')),
+    (_('the %s') % _('Cerro Charrúa'), 5, _('Co. Charrúa'), _('Queda al este'))
+]
+]
+
+LEVEL5 = [
+        1,
+        _('Routes'),
+        ['rutas', 'capitales'],
+        ['capitales', 'ciudades'],
+[
+    (_('the %s') % _('Ruta 3'), 6, _('Ruta 3'), _('Recorre el litoral')),
+    (_('the %s') % _('Ruta 31'), 6, _('Ruta 31'), _('Va hacia Tacuarembó')),
+    (_('the %s') % _('Ruta 4'), 6, _('Ruta 4'), _('Va hacia Artigas')),
+    (_('the %s') % _('Ruta 30'), 6, _('Ruta 30'), _('Está en el límite\ncon Artigas'))
+]
+]
+
+LEVELS = [LEVEL2, LEVEL3, LEVEL4, LEVEL5]
+
