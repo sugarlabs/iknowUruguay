b13386c2165c1947893bbe8f111d603835d07f51
Alan Aguiar <alanjas@hotmail.com>
2012-11-25 20:27:46 -0200

    change paradigm in canelones

---
+++ conozco-uruguay.activity/recursos/4canelones/datos/levels.py
@@ -0,0 +1,145 @@
+﻿# -*- coding: utf-8 -*-
+
+from gettext import gettext as _
+
+LEVEL1 = [
+        1,
+        _('Cities'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        [],
+[
+    (_('the city of %s') % _('Santa Lucía'), 2, _('Santa Lucía'), _('Queda en el límite\ncon San José')),
+    (_('the city of %s') % _('Aguas Corrientes'), 2, _('Aguas Corrientes'), _('Queda en el límite\ncon San José')),
+    (_('the city of %s') % _('Los Cerrillos'), 2, _('Los Cerrillos'), _('Queda al oeste')),
+    (_('the city of %s') % _('Juanicó'), 2, _('Juanicó'), _('Queda al sur de la capital')),
+    (_('the city of %s') % _('Canelones'), 2, _('Canelones'), _('Es la capital del\ndepartamento')),
+    (_('the city of %s') % _('Progreso'), 2, _('Progreso'), _('Queda al oeste')),
+    (_('the city of %s') % _('Las Piedras'), 2, _('Las Piedras'), _('Queda cerca de\Montevideo')),
+    (_('the city of %s') % _('La Paz'), 2, _('La Paz'), _('Queda en el límite\ncon Montevideo')),
+    (_('the city of %s') % _('San Antonio'), 2, _('San Antonio'), _('Queda al centro-norte')),
+    (_('the city of %s') % _('Santa Rosa'), 2, _('Santa Rosa'), _('Queda al centro-norte')),
+    (_('the city of %s') % _('Sauce'), 2, _('Sauce'), _('Queda en el centro')),
+    (_('the city of %s') % _('Toledo'), 2, _('Toledo'), _('Queda en el límite\ncon Montevideo')),
+    (_('the city of %s') % _('San Ramón'), 2, _('San Ramón'), _('Queda al norte')),
+    (_('the city of %s') % _('San Bautista'), 2, _('San Bautista'), _('Queda al centro-norte')),
+    (_('the city of %s') % _('San Jacinto'), 2, _('San Jacinto'), _('Queda en el centro')),
+    (_('the city of %s') % _('Suárez'), 2, _('Suárez'), _('Queda cerca de Pando')),
+    (_('the city of %s') % _('Pando'), 2, _('Pando'), _('Queda cerca de Montevideo')),
+    (_('the city of %s') % _('Empalme Olmos'), 2, _('Empalme Olmos'), _('Queda cerca de Pando')),
+    (_('the health resort of %s') % _('Salinas'), 2, _('Salinas'), _('Es un balneario sobre\nel Río de la Plata')),
+    (_('the city of %s') % _('Tala'), 2, _('Tala'), _('Queda al noreste')),
+    (_('the health resort of %s') % _('Atlántida'), 2, _('Atlántida'), _('Es un balneario importante')),
+    (_('the health resort of %s') % _('Parque del Plata'), 2, _('Parque del Plata'), _('Es un balneario\nde la Costa de Oro')),
+    (_('the health resort of %s') % _('La Floresta'), 2, _('La Floresta'), _('Es un balneario\nde la Costa de Oro')),
+    (_('the city of %s') % _('Migues'), 2, _('Migues'), _('Queda al este')),
+    (_('the city of %s') % _('Montes'), 2, _('Montes'), _('Queda en el límite con\nLavalleja')),
+    (_('the city of %s') % _('Soca'), 2, _('Soca'), _('Queda al sureste')),
+    (_('the health resort of %s') % _('San Luis'), 2, _('San Luis'), _('Es un balneario')),
+    (_('the city of %s') % _('Piedras de Afilar'), 2, _('Piedras de Afilar'), _('Queda al sureste')),
+    (_('the health resort of %s') % _('Lagomar'), 2, _('Lagomar'), _('Queda en Ciudad de la Costa')),
+    (_('the health resort of %s') % _('Solymar'), 2, _('Solymar'), _('Queda en Ciudad de la Costa')),
+    (_('the health resort of %s') % _('El Pinar'), 2, _('El Pinar'), _('Queda en Ciudad de la Costa')),
+    (_('the health resort of %s') % _('Cuchilla Alta'), 2, _('Cuchilla Alta'), _('Es un balneario\nde la Costa de Oro')),
+    (_('the health resort of %s') % _('Jaureguiberry'), 2, _('Jaureguiberry'), _('Queda en el límite\ncon Maldonado')),
+    (_('the health resort of %s') % _('Bello Horizonte'), 2, _('Bello Horizonte'), _('Es un balneario\nde la Costa de Oro')),
+    (_('the %s') % _('Parque Roosvelt'), 2, _('Parque Roosvelt'), _('Queda en el límite\ncon Montevideo'))
+]
+]
+
+LEVEL2 = [
+        1,
+        _('Information'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        ['capitales', 'ciudades'],
+[
+    (_('donde se halló el cementerio\nmás grande de animales\nprehistóricos de Sudamérica'), 2, _('Sauce'), _('Es la cuna de Artigas')),
+    (_('donde está el único\ncementerio de la religión\njudía en Uruguay'), 2, _('La Paz'), _('Queda en el límite con\nMontevideo')),
+    (_('una de las principales\nsedes de la producción\nvitivinícola'), 2, _('Santa Rosa'), _('Queda en el centro')),
+    (_('la ciudad que lleva\nel nombre del presidente\ninterino del Uruguay\nentre 1843 y 1852'), 2, _('Suárez'), _('Queda cerca de Pando')),
+    (_('donde estuvo Hernandarias y\ndestacó la "grande abundancia y\nfertilidad de esta tierra"'), 2, _('Los Cerrillos'), _('Queda al oeste')),
+    (_('un lugar que ha contado\ncon un sinnúmero de exponentes\nde la música y el arte'), 2, _('San Bautista'), _('Queda al centro-norte')),
+    (_('una ciudad cuyo nombre\nsignifica la existencia de un\nsentido de mejora en la\condición humana'), 2, _('Progreso'), _('Queda al cerca de\nMontevideo')),
+    (_('donde hay un monumento a la\nindependencia de la República\nde 8 metros de altura'), 2, _('Migues'), _('Queda al este')),
+    (_('una localidad cuyo nombre\nderiva de la instalación de\nusinas de bombeo, filtrado\ny purificación de agua'), 2, _('Aguas Corrientes'), _('Queda en el límite\ncon San José')),
+    (_('una ciudad cuyo nombre\nse origina de haber sido\nuna antigua zona de explotación\nde canteros de piedra'), 2, _('Las Piedras'), _('Es donde se realizó\nnuna importante batalla por\nla independencia')),
+    (_('un importante centro\ncomercial e industrial\ndel departamento'), 2, _('Pando'), _('Queda cerca de\nMontevideo'))
+]
+]
+
+LEVEL3 = [
+        1,
+        _('Health resorts'),
+        ['lineasDepto', 'capitales', 'ciudades'],
+        ['capitales', 'ciudades'],
+[
+    (_('un balneario con un\nlago ubicado al norte\nde la Av. Giannattasio'), 2, _('Lagomar'), _('El nombre hace referencia\na las aguas salinas\ndel Río de la Plata')),
+    (_('donde se encuentra el\noriginal Museo del Pan'), 2, _('Solymar'), _('El nombre alude al verano')),
+    (_('donde se encuentra el\nAutódromo Víctor Borrat Fabini'), 2, _('El Pinar'), _('Es donde está el primer\npeaje hacia el Este')),
+    (_('un balneario que te\nrecibe con un arco\nsobre la ruta'), 2, _('Salinas'), _('Queda cerca de Atlántida')),
+    (_('uno de los balnearios\nmás concurridos y preferidos\npor los turistas'), 2, _('Atlántida'), _('Queda en una península')),
+    (_('un balneario que está\nsobre el arroyo\nSolís Chico'), 2, _('Parque del Plata'), _('Está el este de Atlántida')),
+    (_('uno de los balnearios\nmás antiguos de la\nCosta de Oro'), 2, _('La Floresta'), _('El nombre hace referencia\na la vegetación')),
+    (_('donde hay un pueblo\nde pescadores'), 2, _('San Luis'), _('Tiene nombre de santo')),
+    (_('uno de los balnearios\nmás pintorescos de la\nCosta de Oro'), 2, _('Cuchilla Alta'), _('Es famoso por su\nchorro de agua dulce\nen la playa')),
+    (_('el balneario que\nse sitúa a orillas del\narrollo Solís Grande'), 2, _('Jaureguiberry'), _('Es el último balneario\nde la Costa de Oro')),
+    (_('donde se corren rallyes\ninternacionales de caballos árabes'), 2, _('Bello Horizonte'), _('Queda cerca de La Floresta')),
+    (_('donde se realizan\nlas criollas en\nSemana de Turismo'), 2, _('Parque Roosvelt'), _('Queda en el límite con\nMontevideo'))
+]
+]
+	
+LEVEL3 = [
+        1,
+        _('Waterways'),
+        ['lineasDepto', 'rios'],
+        [],
+[
+    (_('the %s') % _('Río de la Plata'), 3, _('Río de la Plata'), _('Queda al sur')),
+    (_('the %s') % _('Río Santa Lucía'), 3, _('Río Santa Lucía'), _('Queda al norte y oeste')),
+    (_('the %s') % _('Arroyo Vejigas'), 3, _('A. Vejigas'), _('Queda al norte')),
+    (_('the %s') % _('Arroyo del Tala'), 3, _('A. del Tala'), _('Queda al norte')),
+    (_('the %s') % _('Arroyo Sarandí'), 3, _('A. Sarandí'), _('Queda al este')),
+    (_('the %s') % _('Arroyo Canelón Grande'), 3, _('A. Canelón Gr.'), _('Queda al noroeste')),
+    (_('the %s') % _('Arroyo Canelón Chico'), 3, _('A. Canelón Ch.'), _('Queda al oeste')),
+    (_('the %s') % _('Arroyo Pando'), 3, _('A. Pando'), _('Desemboca en el\nRío de la Plata')),
+    (_('the %s') % _('Arroyo Solís Chico'), 3, _('A. Solís Ch.'), _('Desemboca en el\nRío de la Plata')),
+    (_('the %s') % _('Arroyo Solís Grande'), 3, _('A. Solís Gr.'), _('Queda en el límite\ncon Maldonado')),
+    (_('the %s') % _('Arroyo Mosquitos'), 3, _('A. Mosquitos'), _('Queda al sureste')),
+    (_('the %s') % _('Arroyo Sauce'), 3, _('A. Sauce'), _('Queda al centro')),
+    (_('the %s') % _('Arroyo Brujas Grande'), 3, _('A. Brujas Gr.'), _('Queda al oeste')),
+    (_('the %s') % _('Arroyo de las Piedras'), 3, _('A. de las Piedras'), _('Queda en el límite\ncon Montevideo')),
+    (_('the %s') % _('Arroyo Colorado'), 3, _('A. Colorado'), _('Queda al oeste')),
+    (_('the %s') % _('Arroyo Toledo'), 3, _('A. Toledo'), _('Queda en el límite\ncon Montevideo')),
+    (_('the %s') % _('Arroyo Carrasco'), 3, _('A. Carrasco'), _('Queda en el límite\ncon Montevideo'))
+]
+]
+
+LEVEL4 = [
+        1,
+        _('Elevations'),
+        ['cuchillas', 'cerros'],
+        [],
+[
+    (_('the %s') % _('Cuchilla Grande'), 4, _('Cuchilla Grande'), _('Atraviesa el departamento\npor el centro')),
+    (_('the %s') % _('Cuchilla Cabo de Hornos'), 4, _('Cabo de Hornos'), _('Queda al este'))
+]
+]
+
+LEVEL5 = [
+        1,
+        _('Routes'),
+        ['rutas', 'capitales'],
+        ['capitales', 'ciudades'],
+[
+    (_('the %s') % _('Ruta 5'), 6, _('Ruta 5'), _('Pasa por Canelones')),
+    (_('the %s') % _('Ruta 6'), 6, _('Ruta 6'), _('Pasa por Sauce')),
+    (_('the %s') % _('Ruta 7'), 6, _('Ruta 7'), _('Pasa por San Jacinto')),
+    (_('the %s') % _('Ruta 8'), 6, _('Ruta 8'), _('Pasa por Pando')),
+    (_('the %s') % _('Ruta 9'), 6, _('Ruta 9'), _('Va hacia el este')),
+    (_('the %s') % _('Ruta Interbalnearia'), 6, _('Ruta IB'), _('Pasa por los balnearios')),
+    (_('the %s') % _('Avenida Giannatasio'), 6, _('Av. Giannatasio'), _('Se encuentra con la\nRuta Interbalnearia')),
+    (_('the %s') % _('Ruta 11'), 6, _('Ruta 11'), _('Termina en Atlántida'))
+]
+]
+
+LEVELS = [LEVEL1, LEVEL2, LEVEL3, LEVEL4, LEVEL5]
+
