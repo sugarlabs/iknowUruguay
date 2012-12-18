6d59d8817316e8a15fca4437e6d44ff6a86d1afa
Alan Aguiar <alanjas@hotmail.com>
2012-12-18 16:52:05 -0200

    use new system of rivers in treinta y tres

--- recursos/4treinta_y_tres/datos/levels.py
+++ recursos/4treinta_y_tres/datos/levels.py
@@ -22,31 +22,30 @@ LEVEL2 = [
 ]
 
 LEVEL3 = [
-        1,
+        15,
         _('Waterways'),
         ['lineasDepto', 'rios'],
         [],
 [
-    (_('the %s') % _('Río Olimar Gr.'), 3, _('Río Olimar Gr.'), _("It's in the south")),
-    (_('the %s') % _('Río Olimar Ch.'), 3, _('Río Olimar Ch.'), _("It's in the southwest")),
-    (_('the %s') % _('Río Tacuarí'), 3, _('Río Tacuarí'), _("It's in the northeast")),
-    (_('the %s') % _('Río Cebollatí'), 3, _('Río Cebollatí'), _("It's in the southeast")),
-    (_('the %s') % _('Arroyo Corrales'), 3, _('A. Corrales'), _("It's in the south")),
-    (_('the %s') % _('Arroyo Sarandí Grande'), 3, _('A. Sarandí Gr.'), _("It's in the northeast")),
-    (_('the %s') % _('Arroyo Sarandí Chico'), 3, _('A. Sarandí Ch.'), _("It's in the northeast")),
-    (_('the %s') % _('Arroyo Ayala'), 3, _('A. Ayala'), _("It's in the east")),
-    (_('the %s') % _('Arroyo del Parao'), 3, _('A. del Parao'), _("It's in the north")),
-    (_('the %s') % _('Arroyo Corrales del Parao'), 3, _('A. Corrales del Parao'), _('Está en el centro')),
-    (_('the %s') % _('Arroyo Leoncho'), 3, _('A. Leoncho'), _("It's in the north")),
-    (_('the %s') % _('Arroyo Olazo'), 3, _('A. Olazo'), _("It's in the north")),
-    (_('the %s') % _('Arroyo Yerbal Grande'), 3, _('A. Yerbal Gr.'), _('Está en el centro')),
-    (_('the %s') % _('Arroyo Yerbal Chico'), 3, _('A. Yerbal Ch.'), _('Está en el centro')),
-    (_('the %s') % _('Arroyo de los Porongos'), 3, _('A. de los Porongos'), _('Está en el centro')),
-    (_('the %s') % _('Arroyo de los Ceibos'), 3, _('A. de los Ceibos'), _('Está en el centro')),
-    (_('the %s') % _('Arroyo de las Pavas'), 3, _('A. de las Pavas'), _("It's in the west")),
-    (_('the %s') % _('Arroyo de las Averías'), 3, _('A. de las Averías'), _("It's in the east")),
-    (_('the %s') % _('Arroyo Avestruz Gr.'), 3, _('A. Avestruz Gr.'), _("It's in the west")),
-    (_('the %s') % _('Laguna Merín'), 3, _('Lag. Merín'), _('Está al Está al este'))
+    (_('Olimar Gr.'), _("It's in the south")),
+    (_('Olimar Ch. River'), _("It's in the southwest")),
+    (_('Tacuarí'), _("It's in the northeast")),
+    (_('Cebollatí'), _("It's in the southeast")),
+    (_('Corrales'), _("It's in the south")),
+    (_('Sarandí Gr.'), _("It's in the northeast")),
+    (_('Sarandí Ch.'), _("It's in the northeast")),
+    (_('Ayala'), _("It's in the east")),
+    (_('del Parao'), _("It's in the north")),
+    (_('Corrales del Parao'), _('Está en el centro')),
+    (_('Leoncho'), _("It's in the north")),
+    (_('Olazo'), _("It's in the north")),
+    (_('Yerbal Gr.'), _('Está en el centro')),
+    (_('Yerbal Ch.'), _('Está en el centro')),
+    (_('de los Porongos'), _('Está en el centro')),
+    (_('de los Ceibos'), _('Está en el centro')),
+    (_('de las Pavas'), _("It's in the west")),
+    (_('de las Averías'), _("It's in the east")),
+    (_('Avestruz Gr.'), _("It's in the west"))
 ]
 ]
 
@@ -56,13 +55,13 @@ LEVEL4 = [
         ['cuchillas', 'cerros'],
         [],
 [
-    (_('the %s') % _('Cuchilla Grande'), 4, _('Cuchilla Grande'), _('Dále, probá de nuevo')),
-    (_('the %s') % _('Asperezas del Yerbal'), 4, _('Asperezas del Yerbal'), _('Dále, probá de nuevo')),
-    (_('the %s') % _('Cuchilla del Carmen'), 4, _('Cuchilla del Carmen'), _('Dále, probá de nuevo')),
-    (_('the %s') % _('Cuchilla de Palomeque'), 4, _('Cuchilla de Palomeque'), _('Dále, probá de nuevo')),
+    (_('the %s') % _('Cuchilla Grande'), 4, _('Cuchilla Grande'), _('Try again')),
+    (_('the %s') % _('Asperezas del Yerbal'), 4, _('Asperezas del Yerbal'), _('Try again')),
+    (_('the %s') % _('Cuchilla del Carmen'), 4, _('Cuchilla del Carmen'), _('Try again')),
+    (_('the %s') % _('Cuchilla de Palomeque'), 4, _('Cuchilla de Palomeque'), _('Try again')),
     (_('the %s') % _('Cerro Valentín'), 5, _('Co. Valentín'), _('Está en límite\ncon Florida')),
     (_('the %s') % _('Cerro Chato'), 5, _('Co. Chato'), _("It's in the west")),
-    (_('the %s') % _('Cerro del Olimar'), 5, _('Co. del Olimar'), _('Está en el centro'))
+    (_('the %s') % _('Cerro del Olimar'), 5, _('Co. del Olimar'), _("It's in the center"))
 ]
 ]
 
